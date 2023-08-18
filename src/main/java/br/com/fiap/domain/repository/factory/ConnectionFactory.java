54086d578aa351e67345c20340f2483efdf14e48
logonrmlocal <gabi.stanguini14@gmail.com>
2023-08-18 09:44:59 -0300

    Iniciamos a construção da ConnectionFactory

---
+++ src/main/java/br/com/fiap/domain/repository/factory/ConnectionFactory.java
@@ -0,0 +1,75 @@
+package br.com.fiap.domain.repository.factory;
+
+import javax.xml.transform.Source;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLOutput;
+import java.util.Objects;
+import java.util.Properties;
+
+public class ConnectionFactory {
+
+
+    private static volatile ConnectionFactory instance;
+    // volatile garante que se essa instancia for modificada, ele vai receber a ultima versao o mais rapido possivel
+
+
+    private volatile Connection conexao;
+    private String url;
+    private String user;
+    private String pass;
+    private String driver;
+
+    private ConnectionFactory(String url, String user, String pass, String driver) {
+        this.url = url;
+        this.user = user;
+        this.pass = pass;
+        this.driver = driver;
+    }
+
+    public static ConnectionFactory build() {
+
+        ConnectionFactory factory = instance;
+
+        if (Objects.nonNull(factory)) {
+            return factory;
+        }
+        Properties prop = new Properties();
+        FileInputStream file = null;
+        String debugar = "";
+
+        try {
+            file = new FileInputStream("src/main/resources/application.properties");
+            prop.load(file);
+
+            String url = prop.getProperty("datasource.url");
+            String user = prop.getProperty("datasource.username");
+            String pass = prop.getProperty("datasource.password");
+            String driver = prop.getProperty("datasource.driver-class-name");
+            debugar = prop.getProperty("datasource.debugar");
+
+            file.close();
+
+            if (Objects.nonNull(debugar) && debugar.equals("true")) {
+                System.out.println("*.-".repeat(5) + "CONNECTION PROPERTIES" + "*.-".repeat(5));
+                System.out.println("JDBC URL:" + url);
+                System.out.println("JDBC USER:" + user);
+                System.out.println("JDBC PASSWORD:" + "**********");
+                System.out.println("JDBC DRIVER:" + driver);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Não conseguimos encontrar o arquivo de propriedades: " + e.getMessage());
+        } catch (IOException e) { //se não for permitida a leitura
+            System.err.println("Não conseguimos ler o arquivo de propriedades com o nome:" + e.getMessage());
+        } finally {
+            if (Objects.nonNull(debugar) && debugar.equals("true")) //os dois tem que ser true
+                System.out.println("*.-".repeat(20));
+        }
+        return null;
+    }
+}
+
+
